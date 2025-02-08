d163aa44cbfd26ed2161e4c3293d479547de0193
Arunav <arunavsameer@gmail.com>
2025-02-09 03:47:33 +0530

    Updated server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ PORT = int(os.getenv("PORT", 12345))  # Use Railway's assigned port
 
 # Store sensitive data in environment variables
 FLAG = os.getenv("FLAG", "FLAG{default_flag}")  # Set FLAG in Railway settings
-CORRECT_PASSWORD = "BabyRevEngg"
+CORRECT_PASSWORD = os.getenv("PASSWORD", "default")  # Secure password storage
 
 OBFUSCATED_PASSWORD = "ba 60 a7 ee 1c 73 90 cf 47 9a 17"
 
